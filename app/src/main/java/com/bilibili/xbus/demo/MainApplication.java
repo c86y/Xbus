f450b4450446645746a2d71342003b21e7f5d422
c86y <c86y@163.com>
2016-08-30 00:11:30 +0800

    use freeline

--- app/src/main/java/com/bilibili/xbus/demo/MainApplication.java
+++ app/src/main/java/com/bilibili/xbus/demo/MainApplication.java
@@ -2,6 +2,8 @@ package com.bilibili.xbus.demo;
 
 import android.app.Application;
 
+import com.antfortune.freeline.FreelineCore;
+
 /**
  * @author kaede
  * @version date 16/8/19
@@ -11,6 +13,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        FreelineCore.init(this);
     }
 
     @Override
